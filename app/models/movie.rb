4e59439f438c5702f63bfb82e11158a58d6a312f
Mago Arcera <magocali44@gmail.com>
2023-05-25 12:27:10 +0200

    add restaurant model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,6 @@
 class Movie < ApplicationRecord
+  has_many :bookmarks, dependent: :destroy
+  # validations
+  validates :title, presence: true, uniqueness: true
+  validates :overview, presence: true
 end
